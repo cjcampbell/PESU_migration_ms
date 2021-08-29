d57161c92f01ac8d381392ea1764dc0cfc81f21e
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-08-29 13:59:51 -0400

    minor model upgrade

--- R/09_modelFitting.R
+++ R/09_modelFitting.R
@@ -1,35 +1,43 @@
+
+# Setup -------------------------------------------------------------------
+
 library(MuMIn)
 library(car)
+options(na.action = "na.fail")
+
+myResults <- read.csv( file.path(wd$bin, "myResults.csv") )
+
 df <- myResults %>% 
   filter(Season == "Winter") %>% 
   filter(!is.na(Sex), !is.na(Size), !is.na(Region_long))
 
-m_global <- lm(probDistTraveledAtThreshold_0.25~Sex+Size+Region_long, data = df)
-summary(m_global) 
-car::vif(m_global)
 
-options(na.action = "na.fail")
-m_dredged <- dredge(m_global)
-# View(m_dredged)
+# Fit global model --------------------------------------------------------
 
-# Model weight (after discarding noncompeting models)
-m_dredged_clean <- subset(m_dredged, delta==0|delta>2)
-m_dredged_clean
+# Check out predictor formats and proportions.
+df$Sex  %>% as.character %>% as.factor %>% as.numeric %>% hist
+df$Size %>% as.character %>% as.factor %>% as.numeric %>% hist
+df$Region_long %>% as.character %>% as.factor %>% as.numeric %>% hist
 
-# Check out top model
-topModel <- lm(probDistTraveledAtThreshold_0.25~Size+Region_long, data = df)
-# How much variance is explained?
-out <- performance::r2(topModel) 
+# Check out response dist.
+df$probDistTraveledAtThreshold_0.25 %>% hist
+
+# Fit model.
+m1 <- glm(probDistTraveledAtThreshold_0.25~Sex+Size+Region_long, data = df)
+d1 <- dredge(m1)
+m2 <- glm(probDistTraveledAtThreshold_0.25~Size+Region_long, data = df)
+d2 <- dredge(m2)
+car::vif(m2)
 
 
 # Put model estimates into temporary data.frames:
-model1Frame <- data.frame(Variable = rownames(summary(topModel)$coef),
-                          Coefficient = summary(topModel)$coef[, 1],
-                          SE = summary(topModel)$coef[, 2],
+model1Frame <- data.frame(Variable = rownames(summary(m2)$coef),
+                          Coefficient = summary(m2)$coef[, 1],
+                          SE = summary(m2)$coef[, 2],
                           modelName = "Top Model", row.names = NULL)
-model2Frame <- data.frame(Variable = rownames(summary(m_global)$coef),
-                          Coefficient = summary(m_global)$coef[, 1],
-                          SE = summary(m_global)$coef[, 2],
+model2Frame <- data.frame(Variable = rownames(summary(m1)$coef),
+                          Coefficient = summary(m1)$coef[, 1],
+                          SE = summary(m1)$coef[, 2],
                           modelName = "Global Model",  row.names = NULL)
 
 # Combine these data.frames
@@ -45,4 +53,18 @@ allModeldf <- data.frame(rbind(model1Frame, model2Frame)) %>%
       Variable = factor(Variable, levels = rev(c("Hib. Region (N-C)", "Hib. Size (Small)", "Sex (Male)", "(Intercept)")))
     )
 
-save(m_global, topModel, allModeldf,  file = file.path(wd$bin, "modelResults.Rdata"))
+save(m2, m1, allModeldf,  file = file.path(wd$bin, "modelResults.Rdata"))
+
+
+# check model results  -----------------------------------------------------------
+library(gtsummary)
+library(performance)
+
+# How much variance is explained?
+performance::r2(m2) 
+
+# Table
+gtsummary::tbl_regression(m2)
+
+# Plot resids.
+plot(m2)
